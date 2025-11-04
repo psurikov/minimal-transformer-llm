450a7ee2eb85a776694b8f9a2b84294eea1b2dd3
Pavel Surikov <surikov.pavel@gmail.com>
2025-11-03 17:37:14 -0800

    Added explanations to rope

--- minimal_transformer_llm/rope.py
+++ minimal_transformer_llm/rope.py
@@ -16,6 +16,38 @@ class RotaryPositionalEmbedding(nn.Module):
         self.register_buffer("cos", torch.cos(angles), persistent=False)
     
     def forward(self, x: torch.Tensor, token_positions: torch.Tensor)-> torch.Tensor:
+        # token_positions tensor has shape: [batch_size, seq_len]
+        # or [0,1,2,3,..,len-1], [0,1,2,3,..,len-1], ...
+        # self.cos has shape: [seq_len, d_k // 2]
+        # by performing self.cos[token_positions], this essentially replaces each index in token_positions by respective array of angles in self.cos
+        # resulting in shape [batch_size, seq_len, d_k // 2]
+        # we want to broadcast (multiply element-wise) these values by x1 and x2, which are based on x
+        # assume x has shape [batch_size, heads, seq_len, d_k]
+        # then x1, x2 have shapes [batch_size, heads, seq_len, d_k // 2]
+        # this almost matches the shape of cos, compare:
+        # cos:    [batch_size, seq_len, d_k // 2]
+        # x1, x2: [batch_size, heads, seq_len, d_k // 2]
+        # there is an additional heads dimension here, so simply multiplying these tensors won't work
+        # as a result we need to unsqueeze cos (sin) to include heads dimension, and use 1 just so the dimensions match
+        # this is why .unsqueeze(-3) is used, as a result the shape [batch_size, seq_len, d_k // 2] is transformeed to [batch_size, 1, seq_len, d_k // 2]
+        #
+        # example:
+        # batch_size = 2, seq_len = 4, d_k // 2 = 2
+        # token_positions:  shape 2, 4 (batch_size, seq_len) [[0, 1, 2, 3], [0, 1, 2, 3]]
+        # self.cos:         shape 5, 2 (max_seq_len, d_k // 2)    [[0.1], [0.2]], [[0.3], [0.4]], [[0.5], [0.6]], [[0.7], [0.8]], [[0.9], [1.0]]
+        # cos =             shape 2, 4, 2 (batch_size, seq_len, d_k // 2)
+        # [
+        #   [[0.1], [0.2]], 
+        #   [[0.3], [0.4]], 
+        #   [[0.5], [0.6]], 
+        #   [[0.7], [0.8]]
+        # ], 
+        # [
+        #   [[0.1], [0.2]], 
+        #   [[0.3], [0.4]], 
+        #   [[0.5], [0.6]], 
+        #   [[0.7], [0.8]]
+        # ]
         cos = self.cos[token_positions].unsqueeze(-3)
         sin = self.sin[token_positions].unsqueeze(-3)
         x1, x2 = x[..., ::2], x[..., 1::2]
